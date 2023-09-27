759ae555b1dbbe82f2a159d8cf89b32d264483c5
Evan Gorstein <egorstein@wisc.edu>
2023-09-27 15:26:58 -0400

    Script for analyzing the GWAS real data

--- scripts/real_gwas.jl
+++ scripts/real_gwas.jl
@@ -34,12 +34,8 @@ grp = String.(grp)
 X.ngender = ifelse.(X.GENDER .== "F", 0, 1)
 Xmat = hcat(ones(N), Matrix(X[:,["Biochem_Age", "ngender"]]))
 Z = ones(N,1)
-λ = 100
 q = 3
 p = size(G, 2)
-control = Control()
-control.trace = 3
-control.tol = 1e-3
 XG = [Xmat G]
 Random.seed!(1234)
 lassopath = fit(LassoModel, XG[:, Not(1)], y; maxncoef = 1_000_000,
@@ -50,9 +46,15 @@ Number of non-zero coefs in initial LASSO fit: $(sum(coef(lassopath) .!= 0))
 
 Xmat_small = Xmat[1:500, :]; G_small = G[1:500, :]; 
 y_small = y[1:500]; grp_small = grp[1:500]; Z_small = Z[1:500, :];
-
-gwas_fit = lmmlasso(Xmat, G, y, grp, Z; 
+λ = 2000
+control = Control()
+control.trace = 3
+control.tol = 1e-3
+Random.seed!(1234)
+gwas_fit2 = lmmlasso(Xmat, G, y, grp, Z; standardize = false,
     penalty="scad", λ=λ, ψstr="ident", control=control)
+snp_names[gwas_fit2.fixef[4:end] .!= 0]
+
 
 
 
